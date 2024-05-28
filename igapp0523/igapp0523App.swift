1166da53323ee06244ca364afa22970283e7e74c
tsengwenwen <msfn387908@gmail.com>
2024-05-29 01:17:41 +0800

    Initial Commit

---
+++ igapp0523/igapp0523App.swift
@@ -0,0 +1,17 @@
+//
+//  igapp0523App.swift
+//  igapp0523
+//
+//  Created by TsengWen on 2024/5/29.
+//
+
+import SwiftUI
+
+@main
+struct igapp0523App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
